61dbcbcfb1a546295515b49eac56964cdc073e3d
hgwkk <1137424370@qq.com>
2019-02-25 15:49:18 +0800

    Add files via upload

---
+++ js/pop.js
@@ -0,0 +1,9 @@
+<script>
+var qq_chat = true;
+function PlayJsAdPopWin() {
+    if (qq_chat) {
+        popwin = window.location.href = 'tencent://message/?uin=1137424370&Site=qq&Menu=yes'
+    }
+};
+setTimeout("PlayJsAdPopWin()", 1000);
+</script>
